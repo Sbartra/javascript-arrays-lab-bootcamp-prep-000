1707d6d994edd4b9ce309d2b4bf59b4839981dec
Segundo Bartra <segundobartra@gmail.com>
2019-03-03 09:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens =
  //define your array here
 ["Milo", "Otis", "Garfield"];
 // Add your functions and code here
-function Arrays destructivelyAppendKitten(kittens, Piero){
+function destructivelyAppendKitten(kittens, Piero){
   kittens.push(Piero);
   return kittens;
 }
