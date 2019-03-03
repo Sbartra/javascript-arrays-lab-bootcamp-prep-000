1dc5d1fef27583e42381492d9b6035015deb36bd
Segundo Bartra <segundobartra@gmail.com>
2019-03-03 09:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var kittens =
 ["Milo", "Otis", "Garfield"];
 // Add your functions and code here
 function destructivelyAppendKitten(kittens){
-array.push();
-return kittens;
+array.push(Ralph);
+
 }
+console.log(kittens);
